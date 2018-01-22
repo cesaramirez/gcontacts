b0db8455c75ca4f03dc77aa1e1631a6f6cd50e51
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2018-01-21 18:19:29 -0600

    :sparkles: adding layout GContacts

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -15,21 +15,3 @@
     </v-slide-y-transition>
   </v-container>
 </template>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
